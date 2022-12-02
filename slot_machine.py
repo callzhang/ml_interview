b68d4995fe3e00aad45dc42c0f16637a106392a0
Derek Zen <lei.zhang@stardust.ai>
2022-12-03 01:47:13 +0800

    added best strategy

--- slot_machine.py
+++ slot_machine.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn
-# import plotly.figure_factory as ff
-import plotly.express as px
-from random import sample
 
 st.title('Casino game')
 st.subheader('问题')
 st.info('''假定你走进一家赌场，你的目标是赢得尽量多的金币。前提：
   1. 赌场里面有很多老虎机（可以考虑为无数台），
   2. 每台收益随机（不同的老虎机之间吐出的金币数量随机）
-  3. 对于任意一台老虎机，其每次收益一样（吐出的金币数量一致）
+  3. 对于任意一台老虎机，其每次收益一样（单个老虎机的回报固定）
   4. 现在你有100次机会去玩老虎机
   请设计一个策略，这个策略可以让你的期望收益最大。''')
 
@@ -20,8 +16,11 @@ st.info('''假定你走进一家赌场，你的目标是赢得尽量多的金币
 class Casino:
     def __init__(self, n):
         self.n = n
-        max_reward = np.random.randint(100)
-        self.population = np.random.poisson(max_reward, n)
+        max_reward = np.random.randint(5, 20)
+        population = np.random.poisson(max_reward, n) 
+        population -= int(max_reward / 2)
+        population[population < 0] = 0
+        self.population = population
         self.current = 0
         self.total = 0
 
@@ -36,16 +35,16 @@ class Casino:
     def reset(self):
         self.current = 0
 
-    def sample(self, n):
-        return sample(self.population.tolist(), n)
-
+    def get_sample(self):
+        return self.population.tolist()
+    sample = property(get_sample)
 
 TOTAL_PlAY = 100
 casino = Casino(TOTAL_PlAY)
-st.text(f'前10次样本：{casino.sample(10)} ...')
 
 st.subheader(f'样例代码')
 input_str = '''
+import scipy, statsmodels, distfit, sklearn, pandas as pd, numpy as np
 class RandomPlay:
     def __init__(self, casino, total_play):
         # 传入的casino是整个赌场的类
@@ -54,8 +53,8 @@ class RandomPlay:
         self.total_reward = 0
         self.observed = []
 
-    def play(self):
-        epsilon = 0.2
+    def play(self): # 系统会调用这个函数执行策略
+        epsilon = 0.5
         for i in range(self.total_play):
             r = np.random.random()
             if r < epsilon or i == 0:
@@ -82,8 +81,8 @@ class MyPlay: # 这个类名请不要修改
         self.total_reward = 0
         self.observed = []
 
-    def play(self):
-        epsilon = 0.2
+    def play(self): # 这个函数是必须的
+        epsilon = 0.5
         for i in range(self.total_play):
             r = np.random.random()
             if r < epsilon or i == 0:
@@ -104,36 +103,116 @@ MyPlay = None
 exec(my_code)
 
 # 最优策略
-from scipy.optimize import curve_fit
-from scipy.stats import poisson
-
+from distfit import distfit
 class BestPlay:
     def __init__(self, casino, total_play) -> None:
         self.casino = casino
         self.total_play = total_play
         self.total_reward = 0
         self.observed = []
+        self.played = 0
+
+    def fit_dist(self):
+        dist = distfit(distr=['gamma', 'lognorm', 'beta', 't', 'chi'], smooth=10)
+        dist.fit_transform(np.array(self.observed), verbose=1)
+        score = dist.model['score'] / len(self.observed)
+        return dist, score
 
     def play(self):
         free_play_times = 10
-        max_ = 0
         # 先玩10次
-        for i in range(free_play_times):
-            reward = self.casino.play()
-            self.observed.append(reward)
-            max_ = max(self.observed)
+        for i in range(self.total_play):
+            if i < free_play_times:
+                self.explore()
+                continue
+            
+            # calculate gains and loss, skip if starting to exploit
+            if self.played - len(self.observed) < 3:
+                # fit with curve_fit
+                dist, score = self.fit_dist()
+
+                if score > 1:  # if RSS is too large, continue explore
+                    self.explore()
+                    continue
+                y = range(self.max*5)
+                # proba = dist.predict(y)['y_proba']
+                p_value = dist.model['model'].cdf(y)
+                proba = p_value
+                proba[1:] = p_value[1:]-p_value[:-1]
+                loss = sum([(self.max-y_)*p for y_, p in zip(y, proba) if y_<self.max])
+                steps_left = (self.total_play-self.played)
+                gain = sum([(y_-self.max)*p for y_,p in zip(y, proba) if y_>self.max]) * steps_left
+                if loss > gain and gain>0:
+                    self.exploit()
+                else:
+                    self.explore()
+            else:
+                self.exploit()
+            print(f'Step {self.played}: loss[{loss}] | gain[{gain}] | total: {self.total_reward}')
+        return self.total_reward
+            
+
+    def explore(self):
+        reward = self.casino.play()
+        self.observed.append(reward)
+        self.played += 1
+        self.total_reward += reward
+        print(f'Step {self.played}: explore, reward: {reward}')
+        return reward
+
+    def exploit(self):
+        self.played += 1
+        self.total_reward += self.max
+        return self.max
+    
+    def get_max(self):
+        return max(self.observed)
+
+    max = property(get_max)
         
-        # 开始计算
-        # fit with curve_fit
-        # parameters, cov_matrix = curve_fit(poisson.pmf, bin_middles, entries)
 
-        
+if st.button('执行我的策略'):
+    ph = st.empty()
+    header = ['随机策略', '我的策略', '最优答案', '得分']
+    result = pd.DataFrame(columns=header)
+    ph.table(result)
+    for i in range(10):
+        print(f'进行第{i+1}次测试')
+        casino = Casino(TOTAL_PlAY)
+        play_random = RandomPlay(casino, TOTAL_PlAY)
+        play2 = MyPlay(casino, TOTAL_PlAY)
+        bestplay = BestPlay(casino, TOTAL_PlAY)
+        # 测试随机策略
+        reward1 = play_random.play()
+        print(f'使用随机策略回报: {reward1}')
+        # st.text(f'使用随机策略回报: {reward1}')
+        # 测试我的策略
+        casino.reset()
+        reward2 = play2.play()
+        print(f'我的策略的回报: {reward2}')
+        # st.text(f'我的策略的回报: {reward2}')
+        # 测试最佳结果
+        casino.reset()
+        reward3 = bestplay.play()
+        print(f'最佳回报：{reward3}')
+        # st.text(f'最佳回报：{reward3}')
+        score = int(reward2/reward3*100)
+        print(f'您的算法得分：{int(reward2/reward3*100)}')
+        rewards = pd.Series([reward1, reward2, reward3, score], index=header)
+        result = result.append(rewards, ignore_index=True)
+        ph.table(result)
+    average = result.mean()
+    st.info('平均得分')
+    st.table(average)
 
-if st.button('执行代码'):
-
-    play_random = RandomPlay(casino, TOTAL_PlAY)
-    reward1 = play_random.play()
-    st.text(f'使用随机策略回报: {reward1}')
-    play2 = MyPlay(casino, TOTAL_PlAY)
-    reward2 = play2.play()
-    st.text(f'我的策略的回报: {reward2}')
+    if average['我的策略'] > average['最优答案']:
+        st.balloons()
+        st.text(f'老虎机的回报：{casino.sample} ...')
+        with open('sample.md', 'a') as f:
+            f.write(f'# New record')
+            f.write(casino.sample)
+            f.write(f'my reward: {reward2}, best reward: {reward3}')
+            f.write('```')
+            f.write(input_str)
+            f.write('```')
+            f.write('-'*100)
